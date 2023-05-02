8c6b3ea7bb33353205f0f16ff9054a0fdaefacd0
Lenin Meza <merolhack [at] gmail.com>
2023-05-02 11:53:20 -0600

    :monocle_face: feat(pw1): Repsitories tested

--- pw1/src/test/java/org/bedu/java/backend/pw/persistence/CourseRepositoryTests.java
+++ pw1/src/test/java/org/bedu/java/backend/pw/persistence/CourseRepositoryTests.java
@@ -1,8 +1,8 @@
 package org.bedu.java.backend.pw.persistence;
 
 import org.bedu.java.backend.pw.model.Course;
+import org.bedu.java.backend.pw.model.Student;
 import org.bedu.java.backend.pw.model.Subject;
-import org.bedu.java.backend.pw.persistence.CourseRepository;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,21 +27,34 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CourseRepositoryTests {
 
     @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
     private SubjectRepository subjectRepository;
 
     @Autowired
     private CourseRepository courseRepository;
 
     @BeforeAll
-    void cleanDatabases(){
+    void cleanDatabases() {
+        studentRepository.deleteAll();
         courseRepository.deleteAll();
         subjectRepository.deleteAll();
     }
 
     @Test
     @DisplayName("Smoke test")
-    void smokeTest(){
+    void smokeTest() {
         Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+
+        Student student = new Student();
+        student.setFirstName("Lenin");
+        student.setLastName("Meza");
+        student.setCreatedAt(currentTimestamp);
+        studentRepository.save(student);
+
+        Map<Student, Integer> score = new HashMap<>();
+        score.put(student, Course.NO_SCORE);
 
         Subject subject = new Subject();
         subject.setName("Web Development");
@@ -48,11 +62,14 @@ public class CourseRepositoryTests {
         subjectRepository.save(subject);
 
         Course course = new Course();
+        course.setId(new Random().nextLong());
         course.setCycle("2023");
         course.setSubject(subject);
         course.setCreatedAt(currentTimestamp);
+        course.setScore(score);
         courseRepository.save(course);
 
+        assertNotNull(student.getId());
         assertNotNull(subject.getId());
         assertNotNull(course.getId());
     }
