1cb49d55318142da8e1365c74b8eec16c5f48516
Lenin Meza <merolhack [at] gmail.com>
2023-05-02 08:56:59 -0600

    :sparkles: feat(pw1): Entities tested

--- pw1/src/main/java/org/bedu/java/backend/pw/model/Course.java
+++ pw1/src/main/java/org/bedu/java/backend/pw/model/Course.java
@@ -1,18 +1,18 @@
 package org.bedu.java.backend.pw.model;
 
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.sql.Timestamp;
+import java.util.Map;
 
 @Setter
 @Getter
 @EqualsAndHashCode
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="courses")
 public class Course {
@@ -24,6 +24,16 @@ public class Course {
     @Column(name="cycle")
     @Size(max=10)
     private String cycle;
+
+    @ManyToOne
+    @JoinColumn(name = "subject_id", referencedColumnName = "id")
+    private Subject subject;
+
+    @ElementCollection
+    @CollectionTable(name = "courses_has_students", joinColumns = {@JoinColumn(name = "courses_FK", referencedColumnName = "id")})
+    @MapKeyJoinColumn(name = "students_FK", referencedColumnName = "id")
+    @Column(name = "score")
+    private Map<Student, Integer> score;
 
     @Column(name="created_at")
     private Timestamp createdAt;
